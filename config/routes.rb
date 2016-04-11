bfd3805024b7769be93dc152e46a6b2a76a9b77a
kevineo <kevin.eugene.ong@gmail.com>
2016-04-11 08:32:28 +0800

    Ready to merge with user view

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
+  resources :users
 
 
 >>>>>>> 9ca549c181a1c8e2ffbbbd2be57244f2dfb53323
